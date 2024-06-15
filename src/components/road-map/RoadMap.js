110ee8ed6b89e515d353292d9cca6fcd0343520e
mohamed-ouanjine-codecademy <email.for.tv.elhajji@gmail.com>
2024-06-15 12:28:30 +0100

    *

--- src/components/road-map/RoadMap.js
+++ src/components/road-map/RoadMap.js
@@ -3,11 +3,12 @@ import Section from './components/Section';
 
 function RoadMap() {
   const [roadmapData, setRoadmapData] = useState(null);
+  const [selectedPlan, setSelectedPlan] = useState('technicalAnalysis');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/data/roadmapData.json');
+        const response = await fetch(`/data/${selectedPlan}Plan.json`);
         const data = await response.json();
         setRoadmapData(data);
       } catch (error) {
@@ -16,20 +17,30 @@ function RoadMap() {
     };
 
     fetchData();
-  }, []);
+  }, [selectedPlan]);
+
+  const handlePlanChange = (event) => {
+    setSelectedPlan(event.target.value);
+  };
 
   return (
     <div className="container" style={{ minHeight: '100vh' }}>
       <div className='row mb-5'>
         <div className='col'>
-          <h1>Technical Analysis Learning Plan</h1>
+          <h1>{selectedPlan === 'technicalAnalysis' ? 'Technical Analysis Learning Plan' : 'SMC Learning Plan'}</h1>
+        </div>
+        <div className='col'>
+          <select value={selectedPlan} onChange={handlePlanChange} className="form-select">
+            <option value="technicalAnalysis">Technical Analysis</option>
+            <option value="smc">SMC</option>
+          </select>
         </div>
       </div>
       <div className='row row-cols-1 g-5'>
         {roadmapData &&
           Object.entries(roadmapData).map(([sectionTitle, section], i) => (
-            <div className='col'>
-              <Section key={i} sectionTitle={sectionTitle} section={section} />
+            <div className='col' key={i}>
+              <Section sectionTitle={sectionTitle} section={section} />
             </div>
           ))}
       </div>
